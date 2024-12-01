e21ad31e61fe8e0f5945d9235ba6abdb4961809f
minimiYakwa <qorudalsw@naver.com>
2024-12-01 23:18:27 +0900

    Update_2024_12_01

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
@@ -7,24 +8,67 @@ public class EnemyMove : MonoBehaviour
     Rigidbody2D rigid;
     Animator anim;
     private int nextMove;
+    SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         Think();
+    }
+
+    private void Update()
+    {
+        if (nextMove != 0)
+        {
+            anim.SetBool("isRunning", true);
+            if (nextMove == 1)
+                spriteRenderer.flipX = true;
+            else
+                spriteRenderer.flipX = false;
+        }
+
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        
     }
 
     private void FixedUpdate()
     {
+        // Move 
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
+        //Platform check (Down Platform)
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y );
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 1));
+        
+        //Platform check (Horizontal Platform)
+        Vector2 rightVec = new Vector2(rigid.position.x, rigid.position.y + 1);
+        Debug.DrawRay(rightVec, nextMove == 1 ? Vector3.right : Vector3.left, new Color(0, 1, 1));
 
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        RaycastHit2D rayHit2 = Physics2D.Raycast(rightVec, nextMove == 1 ? Vector3.right : Vector3.left, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null || rayHit2.collider != null)
+        {
+            turn();
+        }
     }
 
     void Think()
     {
-        nextMove = Random.Range(-1, 1);
-        Debug.Log(nextMove);
+        nextMove = Random.Range(-1, 2);
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void turn()
+    {
+        nextMove *= -1;
+        CancelInvoke();
         Invoke("Think", 5);
     }
 }
--- Assets/Scripts/RubyMove.cs
+++ Assets/Scripts/RubyMove.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Mono.Cecil.Cil;
 using UnityEngine;
 
 public class RubyMove : MonoBehaviour
@@ -7,10 +9,13 @@ public class RubyMove : MonoBehaviour
     Rigidbody2D rigid;
     GameObject ruby;
     private float maxSpeed = 5;
-    private float jumpPower = 5;
+    private float jumpPower = 15;
     private bool isjumping = false;
     SpriteRenderer spriteRenderer;
     Animator anim;
+    private bool isDamage = false;
+    private int jumpCount = 0;
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -21,12 +26,13 @@ public class RubyMove : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonUp("Horizontal"))
+
+        if (Input.GetButtonUp("Horizontal") && isDamage == false)
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
-        if (rigid.velocity.normalized.x == 0)
+        if (rigid.velocity.normalized.x == 0 )
         {
             anim.SetBool("isRun", false);
         }
@@ -46,44 +52,85 @@ public class RubyMove : MonoBehaviour
             {
                 return;
             }
-            isjumping = true;
+            if (jumpCount >= 1)
+                isjumping = true;
+            jumpCount++;
             rigid.AddForce(Vector2.up* jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJump", true);
         }
     }
 
     private void FixedUpdate()
-    {
-        // Ruby Move
+    {   
         float h = Input.GetAxisRaw("Horizontal");
 
-        // Debug.Log(h);
-        if (isjumping == false)
-            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        // Ruby Max Speed
-        if (Mathf.Abs(rigid.velocity.x) > maxSpeed)
-        {
-            rigid.velocity = new Vector2(maxSpeed * h, rigid.velocity.y);
-
+        if (isDamage == false){
+            // Ruby Move
+            if (isjumping == false)
+                rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+            else
+                rigid.AddForce(Vector2.right * h * 0.5f, ForceMode2D.Impulse);
+            // Ruby Max Speed
+            if (Mathf.Abs(rigid.velocity.x) > maxSpeed)
+                {
+                    rigid.velocity = new Vector2(maxSpeed * h, rigid.velocity.y);
+                }
         }
+        
 
         // Ground detect
         if (rigid.velocity.y < 0)
         {
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            Vector2 rightVec = new Vector2(rigid.position.x, rigid.position.y + 1);
+            Debug.DrawRay(rightVec, h == 1 ? Vector3.right : Vector3.left, new Color(0, 1, 1));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position,
                 Vector3.down, 1, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayHit2 = Physics2D.Raycast(rightVec, h == 1 ? Vector3.right : Vector3.left,
+                1, LayerMask.GetMask("Platform"));
 
             if (rayHit.collider != null)
             {
                 if (rayHit.distance < 0.5f)
                 {
-                    Debug.Log(rayHit.collider.name);
+                    //Debug.Log(rayHit.collider.name);
                     anim.SetBool("isJump", false);
+                    isDamage = false;
                     isjumping = false;
+                    jumpCount = 0;
                 }
             }
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //Debug.Log("플레이어가 맞았음.");
+            OnDamaged(collision.transform.position);
+        }
+    }
+
+    private void OnDamaged(Vector2 targetPos)
+    {
+        gameObject.layer = 11;
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // Knock-Back direction 
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1)*7, ForceMode2D.Impulse);
+
+        //피격 애니메이션 실행
+        anim.SetTrigger("doDamaged"); 
+        isDamage = true;
+        // 무적시간 설정
+        Invoke("OffDamaged", 3);
+    }
+
+    private void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
     }
 }
